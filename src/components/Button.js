7c30e8b56f4fccf47836381cb31c53b73581a016
Matt Parisien <matthewparisien4@gmail.com>
2021-11-04 16:45:39 -0400

    Add conditional logic to className to render class based on prop occurence

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,7 +4,7 @@ import "components/Button.scss";
 
 export default function Button(props) {
    return (
-      <button class={props.confirm ? 'button--confirm' : ''}>
+      <button className={props.confirm ? 'button button--confirm' : 'button'}>
          {props.children}
       </button>
    )
