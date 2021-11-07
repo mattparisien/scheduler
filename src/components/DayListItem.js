ca223243cf201ef04e2bd96cf1c43ecb30f31dbf
Matt Parisien <matthewparisien4@gmail.com>
2021-11-07 09:02:14 -0500

    Add initial DayListItem component markup

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,1 +1,10 @@
 import React from 'react';
+
+export default function DayListItem(props) {
+  return (
+    <li>
+      <h2 className="text--regular"> {/*day name goes here */} </h2>
+      <h3 className="text--light"> {/* spots remaining for the day go here */}  </h3>
+    </li>
+  )
+}
