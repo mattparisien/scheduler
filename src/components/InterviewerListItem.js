2b909ae22e4e42114e63535f15f08f58f191f5bc
Matt Parisien <matthewparisien4@gmail.com>
2021-11-07 12:09:06 -0500

    Update class var

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -4,16 +4,10 @@ import classNames from 'classnames';
 
 export default function InterviewerListItem(props) {
 
-  const interviewerClasses = classNames('interviewers__item', { 'interviewers__item--selected': props.selected })
-
-  // id:number - the id of the interviewer
-  // name:string - the name of the interviewer
-  // avatar:url - a url to an image of the interviewer
-  // selected:boolean - determines if an interviewer is selected or not and displays the name and applies appropriate styles if selected.
-  // setInterviewer:function - is run when the <InterviewerListItem> is clicked. This function receives the interviewer's id as an argument. It sets the selected interviewer.
+  const interviewerClass = classNames('interviewers__item', { 'interviewers__item--selected': props.selected })
 
   return (
-    <li className={interviewerClasses} onClick={() => props.setInterviewer(props.id)}>
+    <li className={interviewerClass} onClick={() => props.setInterviewer(props.id)}>
     <img
       className="interviewers__item-image"
       src={props.avatar}
@@ -22,4 +16,5 @@ export default function InterviewerListItem(props) {
     {props.selected && props.name}
   </li>
   )
+
 }
