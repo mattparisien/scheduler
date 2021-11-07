dbbb90ea22a6735fc68e25bf4c4db97a2d9abca9
Matt Parisien <matthewparisien4@gmail.com>
2021-11-07 10:30:38 -0500

    Render dayListItem heading content conditionally

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import '../styles/DayListItem.scss'
+import classNames from 'classnames';
 
 
 export default function DayListItem(props) {
+
+  const dayClass = classNames(
+  'day-list__item', 
+  {'day-list__item--selected': props.selected},
+  {'day-list__item--full': props.spots}
+  );
+
+  const formatSpots = function() {
+    if (props.spots === 1) {
+      return '1 spot remaining'
+    }
+
+    if (props.spots === 0) {
+      return 'no spots remaining'
+    }
+
+    if (props.spots > 1) {
+      return `${props.spots} spots remaining`
+    }
+  }
   
   return (
-    <li onClick={() => props.setDay(props.name)} {...props.selected}>
+    <li onClick={() => props.setDay(props.name)} className={dayClass}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">There {props.spots === 1 ? " is 1 spot" : `are ${props.spots} spots`} remaining</h3>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   )
 }
