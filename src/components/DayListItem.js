1e252ea9821de11a11bb7961841a2a4856fac17e
Matt Parisien <matthewparisien4@gmail.com>
2021-11-08 12:55:57 -0500

    Change prop names passed down to DayList component to reflect user input actions

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -26,7 +26,7 @@ export default function DayListItem(props) {
   }
   
   return (
-    <li onClick={props.setDay} className={dayClass}>
+    <li className={dayClass} onClick={props.setDay} selected={props.selected}>
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots()}</h3>
     </li>
