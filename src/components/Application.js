c7e786d3c9d4ddeb88ded73924f7cdf1c635830f
Matt Parisien <matthewparisien4@gmail.com>
2021-11-12 10:22:10 -0500

    Convert mock appointments object to an array using Object.values

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import "components/Application.scss";
 import Button from "../components/Button";
 import DayList from "components/DayList";
+import Appointment from "components/Appointment/index";
 
 export default function Application(props) {
 	const [day, setDay] = useState("Monday");
@@ -24,6 +25,48 @@ export default function Application(props) {
 			spots: 0,
 		},
 	];
+
+	const appointments = {
+		1: {
+			id: 1,
+			time: "12pm",
+		},
+		2: {
+			id: 2,
+			time: "1pm",
+			interview: {
+				student: "Lydia Miller-Jones",
+				interviewer: {
+					id: 3,
+					name: "Sylvia Palmer",
+					avatar: "https://i.imgur.com/LpaY82x.png",
+				},
+			},
+		},
+		3: {
+			id: 3,
+			time: "2pm",
+		},
+		4: {
+			id: 4,
+			time: "3pm",
+			interview: {
+				student: "Archie Andrews",
+				interviewer: {
+					id: 4,
+					name: "Cohana Roy",
+					avatar: "https://i.imgur.com/FK8V841.jpg",
+				},
+			},
+		},
+		5: {
+			id: 5,
+			time: "4pm",
+		},
+	};
+
+	const appointmentsArray = Object.values(appointments);
+	
 	return (
 		<main className='layout'>
 			<section className='sidebar'>
