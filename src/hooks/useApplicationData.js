075fcdfb4ff8b8ec5413abdb35f0380f80c54829
Matt Parisien <matthewparisien4@gmail.com>
2021-11-24 15:41:49 -0500

    Move all logic related to state into useApplicationData hook

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,3 +1,44 @@
 function useApplicationData() {
-  return { state, setDay, bookInterview, cancelInterview }
+	const setDay = day => setState({ ...state, day });
+
+	const bookInterview = function (id, interview) {
+		//update existing appointment slot
+		const appointment = {
+			...state.appointments[id],
+			interview: { ...interview },
+		};
+
+		const appointments = state.appointments;
+		appointments[id] = appointment;
+
+		return axios
+			.put(`http://localhost:8001/api/appointments/${id}`, { interview })
+			.then(result => {
+				setState(prev => ({
+					...prev,
+					appointments: appointments,
+				}));
+			});
+	};
+
+	const deleteInterview = function (id) {
+		const cancelInterview = {
+			...state.appointments[id],
+			interview: null,
+		};
+
+		const appointments = state.appointments;
+		appointments[id] = cancelInterview;
+
+		return axios
+			.delete(`http://localhost:8001/api/appointments/${id}`)
+			.then(result => {
+				setState(prev => ({
+					...prev,
+					appointments: appointments,
+				}));
+			});
+	};
+
+	return { state, setDay, bookInterview, deleteInterview };
 }
