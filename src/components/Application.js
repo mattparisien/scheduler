e397ed8544b94b9d7a6178de94d3582b61cd96a6
Matt Parisien <matthewparisien4@gmail.com>
2021-11-24 15:53:24 -0500

    Import axios requests into useApplicationData hook

--- src/components/Application.js
+++ src/components/Application.js
@@ -14,34 +14,18 @@ export default function Application(props) {
 	let dailyInterviewers = [];
 	let dailyAppts = [];
 
-	const { state, setDay, bookInterview, deleteInterview } = useApplicationData();
-
-	useEffect(() => {
-		Promise.all([
-			axios.get(`http://localhost:8001/api/days`),
-			axios.get(`http://localhost:8001/api/appointments`),
-			axios.get(`http://localhost:8001/api/interviewers`),
-		]).then(all => {
-			setState(prev => ({
-				...prev,
-				days: all[0].data,
-				appointments: all[1].data,
-				interviewers: all[2].data,
-			}));
-		});
-	}, []);
-
-	
-
-	
+	const {
+		state,
+		setDay,
+		bookInterview,
+		deleteInterview,
+	} = useApplicationData();
 
 	dailyAppts = getAppointmentsForDay(state, state.day);
 	dailyInterviewers = getInterviewersForDay(state, state.day);
 
 	const schedule = dailyAppts.map(appointment => {
 		const interviewer = getInterviewer(state, appointment.interview);
-
-		console.log(interviewer);
 
 		return (
 			<Appointment
--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -8,11 +8,28 @@ export default function useApplicationData() {
 		interviewers: {},
 		appointments: {},
 	});
-  
+
+	//Axios requests
+	useEffect(() => {
+		Promise.all([
+			axios.get(`http://localhost:8001/api/days`),
+			axios.get(`http://localhost:8001/api/appointments`),
+			axios.get(`http://localhost:8001/api/interviewers`),
+		]).then(all => {
+			setState(prev => ({
+				...prev,
+				days: all[0].data,
+				appointments: all[1].data,
+				interviewers: all[2].data,
+			}));
+		});
+	}, []);
+
+	//SetDay Function
 	const setDay = day => setState({ ...state, day });
 
+	//Book an interview
 	const bookInterview = function (id, interview) {
-		//update existing appointment slot
 		const appointment = {
 			...state.appointments[id],
 			interview: { ...interview },
@@ -31,6 +48,7 @@ export default function useApplicationData() {
 			});
 	};
 
+	//Delete an interview
 	const deleteInterview = function (id) {
 		const cancelInterview = {
 			...state.appointments[id],
