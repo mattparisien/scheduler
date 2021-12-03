a2eba832c5893e1966a3eda7bcb30153dedabee5
Matt Parisien <matthewparisien4@gmail.com>
2021-12-03 16:25:50 -0500

    Implement testing for deleting interview

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,3 +1,5 @@
+const { createUnionTypeAnnotation } = require("@babel/types");
+
 describe("Appointments", () => {
 	beforeEach(() => {
 		cy.request("GET", "/api/debug/reset");
@@ -25,10 +27,21 @@ describe("Appointments", () => {
 		cy.contains(".appointment__card--show", "Tori Malcolm");
 	});
 
-  it('should cancel an interview', () => {
-    cy.get('[alt="Add"]').first().click();
+	it("should cancel an interview", () => {
+		cy.get('[alt="Add"]').first().click();
 		cy.get('[data-testid="student-name-input"]').type("Lydia Miller-Jones");
-    cy.get('[alt="Sylvia Palmer"]').click();
-    cy.get('.button--danger').contains('Cancel').click();
+		cy.get('[alt="Sylvia Palmer"]').click();
+		cy.get(".button--danger").contains("Cancel").click();
+	});
+
+  it('should delete an interview', () => {
+    cy.get('[alt="Delete"]').first().click({ force: true });
+    cy.get(".button--danger").contains("Confirm").click();
+    cy.contains("Deleting").should("exist");
+    cy.contains("Deleting").should("not.exist");
+    cy.contains(".appointment__card--show", "Archie Cohen")
+    .should("not.exist");
+    
   })
+
 });
