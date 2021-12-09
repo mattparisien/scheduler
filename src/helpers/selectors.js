d59555e6cd7932d03ca0d2d00aefe566c0a57933
Matt Parisien <matthewparisien4@gmail.com>
2021-12-09 10:26:33 -0500

    Remove dead commented out code

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -12,31 +12,14 @@ export function getAppointmentsForDay(state, day) {
 	return appointmentsArray;
 }
 
-// export function getAppointmentsForDay(state, day) {
-// 	const finalAppointments = [];
-// 	const filteredAppointments = state.days.filter(days => days.name === day);
-
-// 	if (filteredAppointments[0]) {
-// 		// console.log('filtered____', filteredAppointments)
-// 		const days = filteredAppointments[0].appointments;
-
-// 		days.forEach(appointment => {
-// 			finalAppointments.push(state.appointments[appointment]);
-// 		});
-// 	}
-// 	return finalAppointments;
-// }
-
 export function getInterviewer(state, interview) {
 	if (interview) {
 		const interviewer = state.interviewers[interview.interviewer];
-		
+
 		const finalInterview = {
 			student: interview.student,
 			interviewer: interviewer,
 		};
-
-		console.log(finalInterview);
 
 		return finalInterview;
 	}
